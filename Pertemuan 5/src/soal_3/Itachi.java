ce1a3a34fd717155fe289b449abba8084a138303
Muhammad Taufik <91010909+mh-taufik@users.noreply.github.com>
2021-10-11 09:02:10 +0700

    Update Pertemuan 5

---
+++ Pertemuan 5/src/soal_3/Itachi.java
@@ -0,0 +1,19 @@
+package soal_3;
+
+public class Itachi {
+	private String KekkeiGenkai = "Susanoo";
+	private String Dojutsu = super.Dojutsu;
+
+	void printKekkeiGenkai() {
+		System.out.println(this.KekkeiGenkai);
+	}
+
+	void printDojutsu() {
+		System.out.println(this.Dojutsu);
+		System.out.println(this.Dojutsu);
+	}
+
+	private void setDojutsu() {
+		this.Dojutsu = "Mangekyou Sharingan";
+	}
+}
---
+++ Pertemuan 5/src/soal_3/Sasuke.java
@@ -0,0 +1,9 @@
+package soal_3;
+
+public class Sasuke {
+	String Dojutsu = "Sharingan";
+
+	void printDojutsu() {
+		System.out.println(this.Dojutsu);
+	}
+}
